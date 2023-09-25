e2f1d66f29242b62d27c426a16fa3cc322e931c3
Alexei Popov <alexeipopov.93@gmail.com>
2023-09-25 11:47:07 +0300

    CUB-P1 initialize express server with folder structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello from index.js');
+const express = require('express');
+const app = express();
+const PORT = 5050;
+
+app.get('/', (req, res) => {
+  res.send('Hello from express js server');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
