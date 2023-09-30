181f58274cbccc1d09d7b1e6852da3e5b5595d52
Alexei Popov <alexeipopov.93@gmail.com>
2023-09-30 09:56:51 +0300

    added mongoose driver, and updated resources

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -4,6 +4,7 @@ const path = require('path');
 const expressConfig = (app) => {
   const staticFiles = express.static(path.resolve(__dirname, '../public'));
   app.use(staticFiles);
+  // middleware that allows us to access the info from FORMS
   app.use(express.urlencoded({ extended: false }));
 };
 
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -2,28 +2,28 @@ const uniqid = require('uniqid');
 
 // cubes database
 const cubes = [
-  {
-    id: '1cww4112qplmyzvqmi',
-    name: 'Alexei Popov',
-    description: '111',
-    imageUrl: 'imageURLTest',
-    difficultyLevel: 1,
-  },
-  {
-    id: '1cww4112qplmyzvzoa',
-    name: 'Gosho',
-    description: 'very nice guy',
-    imageUrl: 'dasd',
-    difficultyLevel: 4,
-  },
-  {
-    id: '1cww4112qplmyzxte6',
-    name: 'Cube3',
-    description: 'colorful cube',
-    imageUrl:
-      'https://images.pexels.com/photos/1500610/pexels-photo-1500610.jpeg?cs=srgb&dl=pexels-jadson-thomas-1500610.jpg&fm=jpg',
-    difficultyLevel: 3,
-  },
+  // {
+  //   id: '1cww4112qplmyzvqmi',
+  //   name: 'Alexei Popov',
+  //   description: '111',
+  //   imageUrl: 'imageURLTest',
+  //   difficultyLevel: 1,
+  // },
+  // {
+  //   id: '1cww4112qplmyzvzoa',
+  //   name: 'Gosho',
+  //   description: 'very nice guy',
+  //   imageUrl: 'dasd',
+  //   difficultyLevel: 4,
+  // },
+  // {
+  //   id: '1cww4112qplmyzxte6',
+  //   name: 'Cube3',
+  //   description: 'colorful cube',
+  //   imageUrl:
+  //     'https://images.pexels.com/photos/1500610/pexels-photo-1500610.jpeg?cs=srgb&dl=pexels-jadson-thomas-1500610.jpg&fm=jpg',
+  //   difficultyLevel: 3,
+  // },
 ];
 
 // cube service functionality
