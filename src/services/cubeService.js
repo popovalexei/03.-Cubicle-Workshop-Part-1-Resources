c720d32286e567c709dc4306ebd711ea9e38364a
Alexei Popov <alexeipopov.93@gmail.com>
2023-09-30 12:07:46 +0300

    make cube create endpoint work with db, remove uniqid dependencie

--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,40 +1,13 @@
-const uniqid = require('uniqid');
+const Cube = require('./../models/Cube.js');
 
 // cubes database
-const cubes = [
-  // {
-  //   id: '1cww4112qplmyzvqmi',
-  //   name: 'Alexei Popov',
-  //   description: '111',
-  //   imageUrl: 'imageURLTest',
-  //   difficultyLevel: 1,
-  // },
-  // {
-  //   id: '1cww4112qplmyzvzoa',
-  //   name: 'Gosho',
-  //   description: 'very nice guy',
-  //   imageUrl: 'dasd',
-  //   difficultyLevel: 4,
-  // },
-  // {
-  //   id: '1cww4112qplmyzxte6',
-  //   name: 'Cube3',
-  //   description: 'colorful cube',
-  //   imageUrl:
-  //     'https://images.pexels.com/photos/1500610/pexels-photo-1500610.jpeg?cs=srgb&dl=pexels-jadson-thomas-1500610.jpg&fm=jpg',
-  //   difficultyLevel: 3,
-  // },
-];
+const cubes = [];
 
 // cube service functionality
-exports.create = (cubeData) => {
-  const newCube = {
-    id: uniqid(),
-    ...cubeData,
-  };
+exports.create = async (cubeData) => {
+  const cube = await Cube.create(cubeData);
 
-  cubes.push(newCube);
-  return newCube;
+  return cube;
 };
 
 exports.getAll = (search, from, to) => {
