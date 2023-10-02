23282c08ff6f958f3ef7ed9ef784b67831840e44
Alexei Popov <alexeipopov.93@gmail.com>
2023-10-02 12:04:10 +0300

    create accessory endpoint, added accessory model, added accessory service

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const accessoryService = require('./../services/accessoryService.js');
 
-router.get('/create', (req, res) => {
+router.get('/create', async (req, res) => {
   //looks up in the views --> accessories --> and take the file create.hbs
   res.render('accessories/create');
+});
+
+router.post('/create', async (req, res) => {
+  const { name, description, imageUrl } = req.body;
+  await accessoryService.create({ name, description, imageUrl });
+
+  res.redirect('/');
 });
 
 module.exports = router;
