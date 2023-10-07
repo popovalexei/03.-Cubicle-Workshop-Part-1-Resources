d1598e8a16b9db4dd7280542e0abc904d21cd536
Alexei Popov <alexeipopov.93@gmail.com>
2023-10-07 12:36:22 +0300

    update cube schema adding user reference, add to every created cube the owner, provide middleware for authentication

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const cookieParse = require('cookie-parser');
+const { auth } = require('../middlewares/authMiddleware.js');
 
 const expressConfig = (app) => {
   app.use(express.static(path.resolve(__dirname, '../public')));
   // middleware that allows us to access the info from FORMS
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParse());
+  app.use(auth);
 };
 
 module.exports = expressConfig;
